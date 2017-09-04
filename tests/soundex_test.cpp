3c6067eb474bafe1406978f1dae6c43e17538a87
Stefano Secondo <stefano.secondo@tomtom.com>
2017-09-04 16:26:35 +0200

    Minor stuff

--- tests/soundex_test.cpp
+++ tests/soundex_test.cpp
@@ -1,9 +1,20 @@
+#import <string>
+
 class Soundex {
+
+public:
+    std::string encode(const std::string& word) const {
+        return word;
+    }
 };
 
 #include <gmock/gmock.h>
 
+using ::testing::Eq;
+
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     Soundex soundex;
-    ASSERT_EQ(5, 6);
+
+    auto encoded = soundex.encode("A");
+    ASSERT_THAT(encoded, Eq("A"));
 }
