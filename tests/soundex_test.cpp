568149b92a17b8eb95f11591ea55de683b922010
Stefano Secondo <stefano.secondo@tomtom.com>
2017-09-04 16:56:10 +0200

    some cleanup

--- tests/soundex_test.cpp
+++ tests/soundex_test.cpp
@@ -1,20 +1,22 @@
-#import <string>
-
-class Soundex {
-
-public:
-    std::string encode(const std::string& word) const {
-        return word;
-    }
-};
-
 #include <gmock/gmock.h>
+#include "../src/soundex.h"
 
 using ::testing::Eq;
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+// fixture class
+class SoundexEncoding : public testing::Test {
+public:
     Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
 
     auto encoded = soundex.encode("A");
-    ASSERT_THAT(encoded, Eq("A"));
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+
+    auto encoded = soundex.encode("I");
+    ASSERT_THAT(encoded, Eq("I000"));
 }
