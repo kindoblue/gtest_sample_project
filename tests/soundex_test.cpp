24b66d3039f77e544ea52ddd1c9a4ca79ceafa6a
Stefano Secondo <stefano.secondo@tomtom.com>
2017-09-06 11:36:30 +0200

    debug version

--- tests/soundex_test.cpp
+++ tests/soundex_test.cpp
@@ -25,4 +25,24 @@ TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
     ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
 }
 
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+    ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
+    ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+    ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+    ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
+
 // https://www.safaribooksonline.com/library/view/modern-c-programming/9781941222423/f_0037.html
